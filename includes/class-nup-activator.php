4f23496a826c3245dc2abbe7c38934f825daa09d
bartholdsen <114864861+bartholdsen@users.noreply.github.com>
2025-06-22 09:42:15 +0200

    Update class-nup-activator.php

--- includes/class-nup-activator.php
+++ includes/class-nup-activator.php
@@ -20,17 +20,31 @@
  * @subpackage Nup/includes
  * @author     Mecum <post@mecum.no>
  */
-class Nup_Activator {
+public static function activate() {
+		// We'll add the new function to create the table here.
+		self::create_entries_table();
+	}
 
 	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.1
+	 * Creates a separate database table to store history.
 	 */
-	public static function activate() {
+	private static function create_entries_table() {
+		global $wpdb;
+		// Gets the table name with the WordPress prefix (e.g. 'wp_nup_entries')
+		$table_name = $wpdb->prefix . 'nup_entries';
+		$charset_collate = $wpdb->get_charset_collate();
 
+		// SQL to create the table
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			timestamp datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			value varchar(255) NOT NULL,
+			PRIMARY KEY  (id)
+		) $charset_collate;";
+
+		// We need to include this file to use dbDelta()
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		dbDelta( $sql );
 	}
 
 }
